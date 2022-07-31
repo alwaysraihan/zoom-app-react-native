4f536a0696107c1c965fec28838d7cc6440ee659
alwaysraihan <bdyoungdeveloper@gmail.com>
2022-07-31 14:18:18 +0600

    Home screen Search Bar ui created

--- src/components/Home/SearchBar.jsx
+++ src/components/Home/SearchBar.jsx
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Fontisto from "react-native-vector-icons/Fontisto";
 
 const SearchBar = () => {
   return (
-    <View>
-      <Text>SearchBar</Text>
+    <View style={styles.container}>
+      <Fontisto name="search" size={20} color="#858585" />
+      <Text style={styles.textSearchBar}>Search</Text>
     </View>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor:"#333333",
+    flexDirection:'row',
+    alignItems:'center',
+    paddingHorizontal:10,
+    height:40,
+    borderRadius:10,
+
+  },
+  textSearchBar:{
+    color:"#858585",
+    paddingLeft:10,
+    fontSize:20
+
+  }
+});
